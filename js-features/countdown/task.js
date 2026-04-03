7fa0fa885546679a2068ddda0f9b1269d3c8fe5c
Vladimir-PC <wimmqm@yandex.ru>
2026-04-03 23:25:54 +0300

    Task 1.2

--- js-features/countdown/task.js
+++ js-features/countdown/task.js
@@ -1,12 +1,28 @@
-const timer = document.getElementById("timer")
+const timer = document.getElementById("timer");
+
+let totalSeconds = Number(timer.textContent);
+
+function formatTime(seconds) {
+    let hours = Math.floor(seconds / 3600);
+    let minutes = Math.floor((seconds % 3600) / 60);
+    let secs = seconds % 60;
+
+    return `${hours.toString().padStart(2, "0")}:${minutes.toString().padStart(2, "0")}:${secs.toString().padStart(2, "0")}`; 
+}
+
+timer.textContent = formatTime(totalSeconds)
+
 
 const interval = setInterval (() => {
-    if (timer.textContent > 0) {
-        timer.textContent -= 1
+    if (totalSeconds > 0) {
+        totalSeconds -= 1
+        timer.textContent = formatTime(totalSeconds)
+
     } else {
         clearInterval(interval)
-
+        alert("Вы победили в конкурсе!")
     }
+    
 }, 1000)
 
-alert("Вы победили в конкурсе!")
+

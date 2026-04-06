f2dd7149bf13f37803ad96382808ee8b84a76b22
Vladimir-PC <wimmqm@yandex.ru>
2026-04-06 20:37:06 +0300

    Задача 2.2

--- js-features/cookie-clicker/task.js
+++ js-features/cookie-clicker/task.js
@@ -1,7 +1,8 @@
 const img = document.getElementsByTagName("img")[0]
 const clickCounter = document.getElementById("clicker__counter")
+const clickerSpeed = document.getElementById("clicker__speed")
 
-
+let lastClickTime = null
 
 img.onclick = function () {
     let clickCounterChanger = Number(clickCounter.textContent)
@@ -12,4 +13,21 @@ img.onclick = function () {
     } else {
         img.width = 200
     }
+
+    const now = new Date()
+
+    if (lastClickTime != null) {
+        const timeDiffSeconds = (now - lastClickTime) / 1000;
+
+        const speed = (1 / timeDiffSeconds).toFixed(2);
+
+        clickerSpeed.textContent = speed;
+    } else {
+
+        clickerSpeed.textContent = "0";
+    }
+
+    lastClickTime = now
+
+
 }

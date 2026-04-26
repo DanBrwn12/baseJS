1def745e5192c98267ffe99e54068e7bbc277038
Vladimir <daway4554@gmail.com>
2026-04-26 18:58:28 +0300

    client-state, Задание 1 (дополнительное)

--- client-state/text-editor/task.js
+++ client-state/text-editor/task.js
@@ -1,4 +1,5 @@
 const editor = document.getElementById('editor')
+const clearButton = document.getElementById('clearButton')
 
 const savedText = localStorage.getItem('editorContent')
 
@@ -8,4 +9,10 @@ if (savedText) {
 
 editor.addEventListener('input', () => {
   localStorage.setItem('editorContent', editor.value)
+})
+
+clearButton.addEventListener('click', () => {
+  editor.value = ''
+  localStorage.removeItem('editorContent')
+  editor.focus()
 })

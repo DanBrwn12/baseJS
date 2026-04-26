d775424455a92b700540ec92f37fb3d805675749
Vladimir <daway4554@gmail.com>
2026-04-26 18:56:34 +0300

    client-state, Задание 1

---
+++ client-state/text-editor/task.js
@@ -0,0 +1,11 @@
+const editor = document.getElementById('editor')
+
+const savedText = localStorage.getItem('editorContent')
+
+if (savedText) {
+  editor.value = savedText
+}
+
+editor.addEventListener('input', () => {
+  localStorage.setItem('editorContent', editor.value)
+})
